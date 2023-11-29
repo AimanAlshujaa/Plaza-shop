b2964b256d11fa3af40a5aa9e69979bb543e4679
AimanAlshujaa <aimanalshogaa3@gmail.com>
2023-11-29 16:43:46 +0300

    عدلت على الnavbar لشاشة الآيباد فقط

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,14 @@
 // Script for navigation bar
+
+
+window.onload = function () {
+    const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+};
+
