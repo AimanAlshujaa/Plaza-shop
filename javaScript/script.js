da4d9dfb6160360a633bfb2f097fc34aaf11fba0
mazen <mazen@gmail.com>
2023-12-11 10:26:42 +0300

    final

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,21 +1,19 @@
 // Script for navigation bar
 
-
 window.onload = function () {
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+  const bar = document.getElementById("bar");
+  const close = document.getElementById("close");
+  const nav = document.getElementById("navbar");
 
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
-}
+  if (bar) {
+    bar.addEventListener("click", () => {
+      nav.classList.add("active");
+    });
+  }
 
-if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
-}
+  if (close) {
+    close.addEventListener("click", () => {
+      nav.classList.remove("active");
+    });
+  }
 };
-
